1f9b0aa1a940705c640f3bfa286e34dc504fc223
OlegaDance <93116261+OlegaDance@users.noreply.github.com>
2023-10-26 13:41:01 +0300

    New contact

--- JAVASCRIPT/Sciripts.js
+++
@@ -1,8 +0,0 @@
-// Отримайте посилання на кнопку
-var createAccountButton = document.getElementById("createAccountButton");
-
-// Додайте обробник події для кнопки
-createAccountButton.addEventListener("click", function() {
-    // Виконайте перехід на іншу сторінку, наприклад, використовуючи location.href
-    window.location.href = "Reg_page.html";
-});
